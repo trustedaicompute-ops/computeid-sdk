49f0171dd51718e53e86bd6bc1c9ad88da6323bd
Praveen Gajjala <trustedaicompute@gmail.com>
2026-06-13 19:22:06 +0300

    bump version to 1.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="computeid-sdk",
-    version="1.1.0",
+    version="1.1.1",
     description="Cryptographic identity for AI compute infrastructure and agentic AI systems",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
